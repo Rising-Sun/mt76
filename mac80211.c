17f13f6a8f42b996299c11254ae503c89bfa16b9
Felix Fietkau <nbd@nbd.name>
2020-07-24 16:11:52 +0200

    mt76: convert from tx tasklet to tx worker thread
    This should improve performance by allowing the scheduler to move the tx
    scheduling work to idle CPUs
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mac80211.c
+++ mac80211.c
@@ -439,15 +439,20 @@ mt76_alloc_device(struct device *pdev, unsigned int size,
 	for (i = 0; i < ARRAY_SIZE(dev->q_rx); i++)
 		skb_queue_head_init(&dev->rx_skb[i]);
 
-	tasklet_init(&dev->tx_tasklet, mt76_tx_tasklet, (unsigned long)dev);
+	if (mt76_worker_setup(dev, &dev->tx_worker, mt76_tx_worker, "tx"))
+		goto free_hw;
 
 	dev->wq = alloc_ordered_workqueue("mt76", 0);
-	if (!dev->wq) {
-		ieee80211_free_hw(hw);
-		return NULL;
-	}
+	if (!dev->wq)
+		goto free_worker;
 
 	return dev;
+
+free_worker:
+	mt76_worker_teardown(&dev->tx_worker);
+free_hw:
+	ieee80211_free_hw(hw);
+	return NULL;
 }
 EXPORT_SYMBOL_GPL(mt76_alloc_device);
 
@@ -500,6 +505,7 @@ EXPORT_SYMBOL_GPL(mt76_unregister_device);
 
 void mt76_free_device(struct mt76_dev *dev)
 {
+	mt76_worker_teardown(&dev->tx_worker);
 	if (dev->wq) {
 		destroy_workqueue(dev->wq);
 		dev->wq = NULL;
